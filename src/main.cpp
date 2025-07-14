885a7baa70cee4db37c309bdc705531adc422858
may.retana <may.retana@ucr.ac.cr>
2025-07-14 12:04:07 -0600

    Agregar comentarios Doxygen a main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
+// Copyright 2025 Jocsan Fernández <jocsan.fernandezsalas@ucr.ac.cr>
+// && Isaac Araya <isaac.arayaquesada@ucr.ac.cr>
+// && May Retana <may.retana@ucr.ac.cr>
 #include <iostream>
 
 #include "Game.hpp"
 
+/**
+ * @brief Función principal del programa
+ *
+ * Inicia el ciclo de vida del objeto Game:
+ * - Inicializa recursos y configuración
+ * - Ejecuta el bucle principal
+ * - Libera los recursos utilizados
+ *
+ * @return int Código de salida del programa
+ */
 int main() {
   Game g;
 
